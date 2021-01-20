a886a03a9b2b6d15a51ee8e8c1a8872cec05f941
Angelo <angelosds@gmail.com>
2021-01-20 00:06:50 -0300

    Added PrimaryButton component

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="primary-button" :type="type" @click="onClick">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class PrimaryButton extends Vue {
+  @Prop({ default: "button" }) private type?: string;
+
+  @Emit()
+  onClick(event: Event) {
+    return event;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.primary-button {
+  background-color: $primary-color;
+  border-radius: 8px;
+  border: 0;
+  color: $text-color-inverse;
+  font-size: 2rem;
+  font-weight: 700;
+  outline: 0;
+  padding: 16px 24px;
+
+  &:hover,
+  &:focus {
+    background-color: $primary-feedback-color;
+  }
+
+  &:focus {
+    box-shadow: inset 0 0 0 4px $primary-color;
+  }
+}
+</style>
---
+++ tests/unit/PrimaryButton.spec.ts
@@ -0,0 +1,48 @@
+import { shallowMount } from "@vue/test-utils";
+
+import PrimaryButton from "@/components/PrimaryButton.vue";
+
+describe("PrimaryButton.vue", () => {
+  const build = (propsData?: object, slots?: object) => {
+    const wrapper = shallowMount(PrimaryButton, {
+      propsData: { ...propsData },
+      slots: { ...slots }
+    });
+
+    return { wrapper };
+  };
+
+  it("renders the component", () => {
+    const { wrapper } = build();
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("renders text in a slot", () => {
+    const defaultSlotText = "default slot text";
+    const { wrapper } = build(undefined, { default: defaultSlotText });
+
+    expect(wrapper.text()).toMatch(defaultSlotText);
+  });
+
+  it("sets a button default type", () => {
+    const { wrapper } = build();
+
+    expect(wrapper.attributes("type")).toEqual("button");
+  });
+
+  it("sets a button custom type", () => {
+    const type = "submit";
+    const { wrapper } = build({ type });
+
+    expect(wrapper.attributes("type")).toEqual(type);
+  });
+
+  it("emits a onClick event", () => {
+    const { wrapper } = build();
+
+    wrapper.trigger("onClick");
+
+    expect(wrapper.emitted("onClick")).toBeTruthy();
+  });
+});
