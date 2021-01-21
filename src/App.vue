ec1f351d3f29d09440da1f7abb205e4fa0e45682
Angelo <angelosds@gmail.com>
2021-01-21 01:22:55 -0300

    Added MainHeader to App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
-  <router-view />
+  <div class="app-container">
+    <MainHeader :placeholder="$t('searchPlaceholder')">{{
+      $t("appTitle")
+    }}</MainHeader>
+    <router-view />
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import MainHeader from "@/components/MainHeader.vue";
+
+@Component({
+  components: {
+    MainHeader
+  }
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 @import "./style/global";
