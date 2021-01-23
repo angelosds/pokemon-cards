a49667ce9799610782134139edbbd61df814d2ca
Angelo <angelosds@gmail.com>
2021-01-23 13:32:45 -0300

    Implemented cards list in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,48 @@
 <template>
-  <div class="home"></div>
+  <div>
+    <grid-carousel>
+      <card
+        v-for="card in cards"
+        :key="card.id"
+        :buttonText="$t('details')"
+        :imageUrl="card.imageUrl"
+        :name="card.name"
+        :number="card.number"
+        :types="card.types"
+        class="grid-carousel--item"
+      />
+    </grid-carousel>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { getModule } from "vuex-module-decorators";
 
-@Component
-export default class Home extends Vue {}
+import Cards from "@/store/cards";
+
+import Card from "@/components/Card.vue";
+import GridCarousel from "@/components/GridCarousel.vue";
+
+@Component({
+  components: {
+    Card,
+    GridCarousel
+  }
+})
+export default class Home extends Vue {
+  private cardsModule = getModule(Cards);
+
+  get cards() {
+    return this.cardsModule.cards;
+  }
+
+  get loading() {
+    return this.cardsModule.loading;
+  }
+
+  mounted() {
+    this.cardsModule.fetAllCards();
+  }
+}
 </script>
